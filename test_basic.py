6b6b772d0d377031b11583cfd94b71c018e11d19
Tanmay Warthe <tanmaywarthe27@gmail.com>
2025-12-20 20:44:23 +0530

    Add test_basic.py

---
+++ test_basic.py
@@ -0,0 +1,14 @@
+import unittest
+import stock_analyzer
+import stock_checker
+
+class TestStockAnalyzer(unittest.TestCase):
+    def test_dummy(self):
+        self.assertTrue(True)
+
+class TestStockChecker(unittest.TestCase):
+    def test_dummy(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
